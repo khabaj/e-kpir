4d765d19887f97e08fd2e0361aed69f4b301335f
Krystian Habaj <habajkrystian@gmail.com>
2016-03-16 20:33:48 +0100

    Initial commit

---
+++ src/main/java/pl/ekpir/EKpirApplication.java
@@ -0,0 +1,12 @@
+package pl.ekpir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EKpirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EKpirApplication.class, args);
+	}
+}
