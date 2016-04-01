c839a7e4dec6d5ccb973fc29873f891a3a66f885
khabaj <habajkrystian@gmail.com>
2016-04-01 14:40:29 +0200

    Frontend changes. Aside menu.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,6 +5,13 @@ NOT EXISTS (
     SELECT role_name FROM role WHERE role_name = 'ADMIN'
 );
 
+insert into role(role_name) 
+SELECT 'USER'
+WHERE
+NOT EXISTS (
+    SELECT role_name FROM role WHERE role_name = 'USER'
+);
+
 insert into user(login,password,role_id) 
 SELECT 'admin', 'admin',1
 WHERE
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,66 +1,21 @@
-angular.module('ekpir', ['ngRoute'])
-    .config(function ($routeProvider, $httpProvider) {
+var app = angular
+		.module('ekpir', [ 'ngRoute' ])
+		.config(
+				function($routeProvider, $httpProvider) {
 
-        $routeProvider.when('/', {
-            templateUrl: 'views/home.html',
-            controller: 'home',
-            controllerAs: 'controller'
-        }).when('/login', {
-            templateUrl: '/views/login.html',
-            controller: 'navigation',
-            controllerAs: 'controller'
-        }).when('/register', {
-            templateUrl: '/views/registration.html',
-            controller: 'navigation',
-            controllerAs: 'controller'
-        }).otherwise('/');
+					$routeProvider.when('/', {
+						templateUrl : 'views/home.html',
+						controller : 'home',
+						controllerAs : 'controller'
+					}).when('/login', {
+						templateUrl : '/views/login.html',
+						controller : 'navigation',
+						controllerAs : 'controller'
+					}).when('/register', {
+						templateUrl : '/views/registration.html',
+						controller : 'navigation',
+						controllerAs : 'controller'
+					}).otherwise('/');
 
-        $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-
-    })
-    .controller('navigation', function ($rootScope, $http, $location) {
-
-        var self = this
-
-        var authenticate = function (credentials, callback) {
-
-            var headers = credentials ? {
-                authorization: "Basic "
-                + btoa(credentials.username + ":" + credentials.password)
-            } : {};
-
-            $http.get('api/users/logged', {headers: headers}).success(function (data) {
-                if (data.name) {
-                    $rootScope.authenticated = true;
-                } else {
-                    $rootScope.authenticated = false;
-                }
-                callback && callback();
-            }).error(function () {
-                $rootScope.authenticated = false;
-                callback && callback();
-            });
-        }
-
-        authenticate();
-        self.credentials = {};
-        self.login = function () {
-            authenticate(self.credentials, function () {
-                if ($rootScope.authenticated) {
-                    $location.path("/");
-                    self.error = false;
-                } else {
-                    $location.path("/login");
-                    self.error = true;
-                }
-            });
-        };
-
-        self.logout = function () {
-            $http.post('logout', {}).finally(function () {
-                $rootScope.authenticated = false;
-                $location.path("/");
-            });
-        }
-    });
-    
+					$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+				});
